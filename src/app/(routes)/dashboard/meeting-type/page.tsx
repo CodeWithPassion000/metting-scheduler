3280416ce8497fa91f82d07cb2c1d3575fbeaf36
CodeWithPassion000 <shivakumararadyasm5@gmail.com>
2024-04-07 13:26:02 +0530

    meeting lising started

--- src/app/(routes)/dashboard/meeting-type/page.tsx
+++ src/app/(routes)/dashboard/meeting-type/page.tsx
@@ -1,7 +1,43 @@
-import React from 'react';
+'use client';
+
+import { Input } from '@/components/ui/input';
+import { collection, getDocs, getFirestore, query } from 'firebase/firestore';
+import React, { useEffect } from 'react';
+import { app } from '../../../../../config/FirebaseConfig';
+import { useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs';
+import { where } from 'firebase/firestore/lite';
 
 function page() {
-  return <div>Meeting type</div>;
+  const db = getFirestore(app);
+
+  const { user } = useKindeBrowserClient();
+
+  useEffect(() => {
+    user && getEventList();
+  }, []);
+
+  const getEventList = async () => {
+    const q = query(
+      collection(db, 'MeetingEvent'),
+      where('createdBy', '==', user?.email)
+    );
+
+    const querySnapshot = await getDocs(q);
+
+    querySnapshot.forEach((doc) => {
+      console.log(doc.id, ' => ', doc.data());
+    });
+  };
+
+  return (
+    <div className="p-5">
+      <div className="flex flex-col gap-5">
+        <h2 className="font-bold text-3xl">Meeting Event type</h2>
+        <Input placeholder="Search" className="max-w-xs" />
+        <hr></hr>
+      </div>
+    </div>
+  );
 }
 
 export default page;
